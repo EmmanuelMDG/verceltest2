b6f2f907567d48454d9ef40578d116bc18ad5cd9
Emmanuel MINDONGO <mdg.m.emmanuel@gmail.com>
2023-12-09 00:15:04 +0100

    tp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ci-cd-introduction'`, () => {
+  it(`should have as title 'emmaPortFolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ci-cd-introduction');
+    expect(app.title).toEqual('emmaPortFolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ci-cd-introduction app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Mindongo Emma');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ci-cd-introduction';
+export class AppComponent {title = 'emmaPortFolio';
+name = 'Mindongo Emma';
+classe = 'Inge 4 ISI';
+role = 'Backend Développeur';
 }
